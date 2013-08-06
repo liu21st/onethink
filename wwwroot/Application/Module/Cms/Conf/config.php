72a2fec0465b83abce984fc081ceae34c1f6637b
麦当苗儿 <zuojiazi.cn@gmail.com>
2013-08-06 10:40:35 +0800

    调整架构

--- wwwroot/Application/Module/Cms/Conf/config.php
+++ wwwroot/Application/Module/Cms/Conf/config.php
@@ -53,7 +53,7 @@ return array(
     
     /* 模板相关配置 */
     'TMPL_PARSE_STRING' => array(
-        '__COMMON__' => __ROOT__ . '/Public/common',
+        '__STATIC__' => __ROOT__ . '/Public/static',
         '__ADDONS__' => __ROOT__ . '/Public/Cms/Addons',
         '__IMG__'    => __ROOT__ . '/Public/Cms/images',
         '__CSS__'    => __ROOT__ . '/Public/Cms/css',
