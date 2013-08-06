213d61497a2d925242a64b739c68dff50fe6e4e4
麦当苗儿 <zuojiazi@vip.qq.com>
2013-08-06 09:56:19 +0800

    删除重复的配置项

--- wwwroot/Application/Conf/config.php
+++ wwwroot/Application/Conf/config.php
@@ -19,7 +19,6 @@ return array(
     'SHOW_PAGE_TRACE' => true,
     
     /* URL配置 */
-    'VAR_URL_PARAMS'       => false, //PATHINFO URL参数变量
     'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
     'URL_MODEL'            => 3, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
