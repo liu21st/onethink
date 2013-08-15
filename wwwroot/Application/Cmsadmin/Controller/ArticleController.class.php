c48a18792c9525f1e103be41f8cae6bb0184c75f
花杰 <banhuajie@163.com>
2013-08-15 14:07:19 +0800

    内容管理列表页初始化

--- wwwroot/Application/Cmsadmin/Controller/ArticleController.class.php
+++ wwwroot/Application/Cmsadmin/Controller/ArticleController.class.php
@@ -4,21 +4,35 @@
 // +----------------------------------------------------------------------
 // | Copyright (c) 2006-2012 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
-// | Author: 麦当苗儿 <zuojiazi.cn@gmail.com> <http://www.zjzit.cn>
+// | Author: huajie <banhuajie@163.com>
 // +----------------------------------------------------------------------
 
 /**
  * 后台内容控制器
- * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+ * @author huajie <banhuajie@163.com>
  */
 
 class ArticleController extends CmsadminController {
 	
 	/**
 	 * 内容管理首页
-	 * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+	 * @author huajie <banhuajie@163.com>
 	 */
-	public function index(){
+	public function index($cate_id = null){
+		if(empty($cate_id)){
+			$this->error('文章分类不能为空');
+		}
+		$Document = D('Ducument');
+		
+		/*初始化分页类*/
+		import('COM.Page');
+		$count = $Document->listCount($cate_id, array('gt', -1));
+		$Page = new Page($count, 20);
+		$this->page = $Page->show();
+		
+		//列表数据获取
+		$list = $Document->lists($cate_id, 'id DESC', array('gt', -1), 'id,uid,title,create_time,status', $Page->firstRow. ',' . $Page->listRows);
+		
 		$this->diaplay();
 	}
 
