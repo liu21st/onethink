82d945ddc7dc89f5bdd8b3f1d18012cccfae7ed7
麦当苗儿 <zuojiazi.cn@gmail.com>
2013-08-15 21:15:09 +0800

    调整入口注释

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -7,14 +7,26 @@
 // | Author: 麦当苗儿 <zuojiazi.cn@gmail.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
 
-// 系统调试设置
+/**
+ * 系统调试设置
+ * 项目正式部署后请设置为false
+ */
 define ( 'APP_DEBUG', true );
  
-// 应用目录设置
+/**
+ * 应用目录设置
+ * 安全期间，建议安装调试完成后移动到非WEB目录
+ */
 define ( 'APP_PATH', './Application/' );
 
-// 缓存目录设置
+/**
+ * 缓存目录设置
+ * 此目录必须可写，建议移动到非WEB目录
+ */
 define ( 'RUNTIME_PATH', './Runtime/' );
 
-// 引入核心入口
+/**
+ * 引入核心入口
+ * ThinkPHP亦可移动到WEB以外的穆里
+ */
 require './ThinkPHP/ThinkPHP.php';
