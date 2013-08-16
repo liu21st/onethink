512bc39cb6a912d2379c3e9c6e3d751c7265627c
麦当苗儿 <zuojiazi.cn@gmail.com>
2013-08-16 14:54:19 +0800

    添加子节点默认定义

--- wwwroot/Application/Admin/Controller/AdminController.class.php
+++ wwwroot/Application/Admin/Controller/AdminController.class.php
@@ -178,7 +178,7 @@ class AdminController extends Action {
      * 
      * @author 朱亚杰  <zhuyajie@topthink.net>
      */
-    final protected function getDeny( array $deny = array())
+    final protected function getDeny()
     {
         $controller = CONTROLLER_NAME.'Controller';
         $data = array();
@@ -247,7 +247,8 @@ class AdminController extends Action {
 //        if ( S('menu'.$controller) ) {
 //            return S('menu'.$controller);
 //        }
-        $menus['main']  = self::$menus;                       //获取主节点
+        $menus['main']  = self::$menus; //获取主节点
+        $menus['child'] = array(); //设置子节点
 
         //处理其他控制器中的节点
         foreach ($menus['main'] as $key=>$item){
