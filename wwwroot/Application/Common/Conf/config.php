a3314b16be3db04526152682120373a9fc38359d
zhuyaie <xcoolcc@gmail.com>
2013-08-19 09:08:39 +0800

    url模式调整

--- wwwroot/Application/Common/Conf/config.php
+++ wwwroot/Application/Common/Conf/config.php
@@ -21,9 +21,9 @@ return array(
     
     /* URL配置 */
     'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
-    'URL_MODEL'            => 3, //URL模式
+    'URL_MODEL'            => 1, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
-    'URL_PATHINFO_DEPR'    => '-', //PATHINFO URL分割符
+    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
