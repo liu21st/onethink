e1edc837db94e38f9ca21b90984f53692f0c1d67
zhuyaie <xcoolcc@gmail.com>
2013-08-21 10:26:35 +0800

    完成用户所属用户组的获取和修改

--- wwwroot/ThinkPHP/Extend/Library/ORG/Util/Auth.class.php
+++ wwwroot/ThinkPHP/Extend/Library/ORG/Util/Auth.class.php
@@ -75,7 +75,7 @@ class Auth{
         'AUTH_ON'           => true,                      // 认证开关
         'AUTH_TYPE'         => 1,                         // 认证方式，1为实时认证；2为登录认证。
         'AUTH_GROUP'        => 'think_auth_group',        // 用户组数据表名
-        'AUTH_GROUP_ACCESS' => 'think_member',            // 用户-用户组关系表
+        'AUTH_GROUP_ACCESS' => 'think_auth_group_access', // 用户-用户组关系表
         'AUTH_RULE'         => 'think_auth_rule',         // 权限规则表
         'AUTH_USER'         => 'think_member'             // 用户信息表
     );
