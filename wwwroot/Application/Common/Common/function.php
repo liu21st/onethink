8e9848f836c8d09fd80b8d7d42f1026967d8fc8d
huajie <banhuajie@163.com>
2013-08-23 14:40:28 +0800

    增加行为函数

--- wwwroot/Application/Common/Common/function.php
+++ wwwroot/Application/Common/Common/function.php
@@ -412,32 +412,83 @@ function ubb($data){
 	return $data;
 }
 
+/**
+ * 记录行为日志，并执行该行为的规则
+ * @param string $action 行为标识
+ * @param string $model 触发行为的表名（不加表前缀）
+ * @param int $record_id 触发行为的记录id
+ * @param int $user_id 执行行为的用户id
+ * @return boolean
+ * @author huajie <banhuajie@163.com>
+ */
 function action_log($action = null, $model = null, $record_id = null, $user_id = null){
+
 	//参数检查
 	if(empty($action) || empty($model) || empty($record_id)){
 		return false;
 	}
+	if(empty($user_id)){
+		$user_id = is_login();
+	}
 
+	//查询行为,判断是否执行
+	$action_info = M('Action')->getByName($action);
+	if($action_info['status'] != 1){
+		return false;
+	}
 
+	//插入行为日志
+	$data['action_id'] = $action_info['id'];
+	$data['user_id'] = $user_id;
+	$data['action_ip'] = ip2long(get_client_ip());
+	$data['model'] = $model;
+	$data['record_id'] = $record_id;
+	$data['create_time'] = NOW_TIME;
+	M('ActionLog')->add($data);
+
+	//解析行为
+	$rules = parse_action($action, $user_id);
+
+	//执行行为
+	$res = execute_action($rules, $action_info['id'], $user_id);
 }
 
-function parse_action($action = null){
+/**
+ * 解析行为规则
+ * 规则定义  table:$table|field:$field|condition:$condition|rule:$rule[|cycle:$cycle|max:$max][;......]
+ * 规则字段解释：table->要操作的数据表，不需要加表前缀；
+ * 				field->要操作的字段；
+ * 				condition->操作的条件，目前支持字符串，默认变量{$self}为执行行为的用户
+ * 				rule->对字段进行的具体操作，目前支持加、减
+ * 				cycle->执行周期，单位（分钟），表示$cycle分钟内执行$max次
+ * 				max->单个周期内的最大执行次数（$cycle和$max必须同时定义，否则无效）
+ * 单个行为后可加 ； 连接其他规则
+ * @param string $action 行为id或者name
+ * @param int $self 替换规则里的变量为执行用户的id
+ * @return boolean|array: false解析出错 ， 成功返回规则数组
+ * @author huajie <banhuajie@163.com>
+ */
+function parse_action($action = null, $self){
 	if(empty($action)){
 		return false;
 	}
+
 	//参数支持id或者name
 	if(is_numeric($action)){
 		$map = array('id'=>$action);
 	}else{
 		$map = array('name'=>$action);
 	}
+
 	//查询行为信息
 	$info = M('Action')->where($map)->find();
 	if(!$info || $info['status'] != 1){
 		return false;
 	}
+
 	//解析规则:table:$table|field:$field|condition:$condition|rule:$rule[|cycle:$cycle|max:$max][;......]
 	$rules = $info['rule'];
+	$rules = str_replace('{$self}', $self, $rules);
 	$rules = explode(';', $rules);
 	$return = array();
 	foreach ($rules as $key=>&$rule){
@@ -454,6 +505,41 @@ function parse_action($action = null){
 			unset($return[$key]['max']);
 		}
 	}
-	dump($return);exit;
+
+	return $return;
+}
+
+/**
+ * 执行行为
+ * @param array $rules 解析后的规则数组
+ * @param int $action_id 行为id
+ * @param array $user_id 执行的用户id
+ * @return boolean false 失败 ， true 成功
+ * @author huajie <banhuajie@163.com>
+ */
+function execute_action($rules = false, $action_id = null, $user_id = null){
+	if(!$rules || empty($action_id) || empty($user_id)){
+		return false;
+	}
+
+	$return = true;
+	foreach ($rules as $rule){
+
+		//检查执行周期
+		$map = array('action_id'=>$action_id, 'user_id'=>$user_id);
+		$map['create_time'] = array('gt', NOW_TIME - intval($rule['cycle']) * 3600);
+		$exec_count = M('ActionLog')->where($map)->count();
+		if($exec_count > $rule['max']){
+			continue;
+		}
+
+		//执行数据库操作
+		$Model = M(ucfirst($rule['table']));
+		$field = $rule['field'];
+		$res = $Model->where($rule['condition'])->setField($field, array('exp', $field.'+'.$rule['rule']));
+		if(!$res){
+			$return = false;
+		}
+	}
 	return $return;
 }
