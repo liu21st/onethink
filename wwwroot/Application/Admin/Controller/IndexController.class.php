1f19db1a436913d24b6ea43f9ccfe13f0c71b9d1
麦当苗儿 <zuojiazi.cn@gmail.com>
2013-08-27 10:04:32 +0800

    调整登录流程

--- wwwroot/Application/Admin/Controller/IndexController.class.php
+++ wwwroot/Application/Admin/Controller/IndexController.class.php
@@ -48,10 +48,33 @@ class IndexController extends AdminController {
      * 后台用户登录
      * @author 麦当苗儿 <zuojiazi@vip.qq.com>
      */
-    public function login(){
+    public function login($username = null, $password = null){
         if(IS_POST){
-            //TODO: 后台用户登录
-            //
+            /* 检测验证码 TODO: */
+            // if(!check_verify($verify)){
+            //     $this->error('验证码输入错误！');
+            // }
+
+            /* 调用UC登录接口登录 */
+            $uid = A('User/User', 'Api')->login($username, $password);
+            if(0 < $uid){ //UC登录成功
+                /* 登录用户 */
+                $Member = D('Member');
+                if($Member->login($uid)){ //登录用户
+                    //TODO:跳转到登录前页面
+                    $this->success('登录成功！', U('Index/index'));
+                } else {
+                    $this->error($Member->getError());
+                }
+
+            } else { //登录失败
+                switch($uid) {
+                    case -1: $error = '用户不存在或被禁用！'; break; //系统级别禁用
+                    case -2: $error = '密码错误！'; break;
+                    default: $error = '未知错误！'; break; // 0-接口参数错误（调试阶段使用）
+                }
+                $this->error($error);
+            }
         } else {
             $this->display();
         }
