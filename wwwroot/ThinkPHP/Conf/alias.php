5964148ca1b4b3de47c595adb8081849526efb1b
ThinkPHP <thinkphp@qq.com>
2013-09-05 08:47:29 +0800

    标签库驱动目录调整 核心入口文件和runtime文件合并

--- wwwroot/ThinkPHP/Conf/alias.php
+++ wwwroot/ThinkPHP/Conf/alias.php
@@ -10,15 +10,18 @@
 // +----------------------------------------------------------------------
 
 defined('THINK_PATH') or exit();
-// 系统别名定义文件
 return array(
-    'Model'         => CORE_PATH.'Core/Model.class.php',
-    'Db'            => CORE_PATH.'Core/Db.class.php',
-    'Log'          	=> CORE_PATH.'Core/Log.class.php',
-    'ThinkTemplate' => CORE_PATH.'Template/ThinkTemplate.class.php',
-    'ThinkStorage'  => CORE_PATH.'Core/ThinkStorage.class.php',    
-    'TagLib'        => CORE_PATH.'Template/TagLib.class.php',
-    'Cache'         => CORE_PATH.'Core/Cache.class.php',
-    'Widget'        => CORE_PATH.'Core/Widget.class.php',
-    'TagLibCx'      => CORE_PATH.'Driver/TagLib/TagLibCx.class.php',
+    'Think\App'               => CORE_PATH . 'App.class.php',
+    'Think\Log'               => CORE_PATH . 'Log.class.php',
+    'Think\Log\Driver\File'   => CORE_PATH . 'Log/Driver/File.class.php',
+    'Think\Exception'         => CORE_PATH . 'Exception.class.php',
+    'Think\Model'             => CORE_PATH . 'Model.class.php',
+    'Think\Db'                => CORE_PATH . 'Db.class.php',
+    'Think\Template'          => CORE_PATH . 'Template.class.php',
+    'Think\Cache'             => CORE_PATH . 'Cache.class.php',
+    'Think\Cache\Driver\File' => CORE_PATH . 'Cache/Driver/File.class.php',
+    'Think\Storage'           => CORE_PATH . 'Storage.class.php',
+    'Think\Action'            => CORE_PATH . 'Action.class.php',
+    'Think\View'              => CORE_PATH . 'View.class.php',
+    'Think\Template\TagLib\Cx'=> CORE_PATH . 'Template/TagLib/Cx.class.php',
 );
