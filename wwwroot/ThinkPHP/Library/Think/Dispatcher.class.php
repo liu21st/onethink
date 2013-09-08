84d9385ebd59912f1b82c47fd281d4327a0f3377
ThinkPHP <thinkphp@qq.com>
2013-09-08 12:47:11 +0800

    去掉调试信息

--- wwwroot/ThinkPHP/Library/Think/Dispatcher.class.php
+++ wwwroot/ThinkPHP/Library/Think/Dispatcher.class.php
@@ -181,7 +181,7 @@ class Dispatcher {
             $var  =  array();
             preg_replace_callback('/(\w+)\/([^\/]+)/', function($match) use(&$var){$var[$match[1]]=strip_tags($match[2]);}, implode('/',$paths));
             $_GET   =  array_merge($var,$_GET);
-        }dump($_GET);
+        }
         define('CONTROLLER_NAME',   self::getController($varController));
         define('ACTION_NAME',       self::getAction($varAction));
         
