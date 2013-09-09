0c264c0f103cd752f978b8f1c5bb6b1be73b2ac7
zhuyaie <xcoolcc@gmail.com>
2013-09-09 17:41:56 +0800

    模板调整

--- wwwroot/Public/Admin/js/common.js
+++ wwwroot/Public/Admin/js/common.js
@@ -17,4 +17,76 @@
 		});
 	});
 
+    //ajax get请求
+    $('.ajax-get').click(function(){
+        var target;
+        if ( $(this).hasClass('confirm') ) {
+            if(!confirm('确认要执行该操作吗?')){
+                return false;
+            }
+        }
+        if ( (target = $(this).attr('href')) || (target = $(this).attr('url')) ) {
+            $.get(target).success(function(data){
+                if (data.status==1) {
+                    location.reload();
+                }else{
+                    //TODO: 错误提示
+                }
+            });
+          
+        }
+        return false;
+    });
+
+    //ajax post请求
+    $('.ajax-post').click(function(){
+        var target,query,form;
+        var target_form = $(this).attr('target-form');
+        if ( $(this).hasClass('confirm') ) {
+            if(!confirm('确认要执行该操作吗?')){
+                return false;
+            }
+        }
+        if ( (target = $(this).attr('href')) || (target = $(this).attr('url')) ) {
+            form = $('.'+target_form);
+            if ( form.get(0).nodeName=='FORM' || form.get(0).nodeName=='INPUT' || form.get(0).nodeName=='SELECT' || form.get(0).nodeName=='TEXTAREA') {
+                query = form.serialize();
+            }else{
+                query = form.find('input,select,textarea').serialize();
+            }
+            $.post(target,query).success(function(data){
+                if (data.status==1) {
+                    location.reload();
+                }else{
+                    //todo: 错误提示
+                }
+            });
+        }
+        return false;
+    });
+
+    //ajax post submit请求
+    $('.save-form').click(function(){
+        var target,query,form;
+        var target_form = $(this).attr('target-form');
+        if( (this.type=='submit') || (target = $(this).attr('href')) || (target = $(this).attr('url')) ){
+            form = $('.'+target_form);
+            if ( form.get(0).nodeName=='FORM' ){
+                target = form.get(0).action;
+                query = form.serialize();
+            }else if( form.get(0).nodeName=='INPUT' || form.get(0).nodeName=='SELECT' || form.get(0).nodeName=='TEXTAREA') {
+                query = form.serialize();
+            }else{
+                query = form.find('input,select,textarea').serialize();
+            }
+            $.post(target,query).success(function(data){
+                if (data.status==1) {
+                    location.reload();
+                }else{
+                    //TODO: 错误提示
+                }
+            });
+        }
+        return false;
+    });
 });
