59a6cd6c725cb9dc87afb163d512015ded53e366
yuwang <648795318@qq.com>
2013-09-09 11:02:04 +0800

    修改前台公共js缺少闭包

--- wwwroot/Public/Home/js/common.js
+++ wwwroot/Public/Home/js/common.js
@@ -8,8 +8,9 @@ $(function(){
 
 	$('.action .detailed').each(function(){
 		$(this).click(function() {
-        detailed_content();
-        return false;
+        	detailed_content();
+        	return false;
+        });
   	});
 
 	var $nav = $("#nav"), $current = $nav.children("[data-key=" + $nav.data("key") + "]");
