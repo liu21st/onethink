b79245bdb0b157e7bba31a27668ac0061977a4dd
huajie <banhuajie@163.com>
2013-09-11 17:45:05 +0800

    增加修改用户信息的方法

--- wwwroot/Application/User/Api/UserApi.class.php
+++ wwwroot/Application/User/Api/UserApi.class.php
@@ -81,4 +81,16 @@ class UserApi extends Api{
 		return $this->model->checkField($mobile, 3);
 	}
 
+	/**
+	 * 更新用户信息
+	 * @param int $uid 用户id
+	 * @param string $password 密码，用来验证
+	 * @param array $data 修改的字段数组
+	 * @return true 修改成功，false 修改失败
+	 * @author huajie <banhuajie@163.com>
+	 */
+	public function updateInfo($uid, $password, $data){
+		return $this->model->updateUserFields($uid, $password, $data);
+	}
+
 }
