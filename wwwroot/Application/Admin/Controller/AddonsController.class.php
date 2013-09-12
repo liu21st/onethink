4ff074a58ae770a59e11fb0cf8fdd1e1b82824b2
jay <917647288@qq.com>
2013-09-12 13:42:02 +0800

    修改插件后台钩子列表处和插件后台管理页面的顺序

--- wwwroot/Application/Admin/Controller/AddonsController.class.php
+++ wwwroot/Application/Admin/Controller/AddonsController.class.php
@@ -257,9 +257,7 @@ str;
             $fields = '*';
         if(!$map)
             $map = array();
-        if(!$order)
-            $order = array();
-        $list = $this->lists(D("Addons://{$model}/{$model}")->field($fields),$map,$order);
+        $list = $this->lists(D("Addons://{$model}/{$model}")->field($fields),$map);
         $thead = array(
             //元素value中的变量就是数据集中的字段,value必须使用单引号
 
@@ -402,8 +400,8 @@ str;
      */
     public function hooks(){
         $this->meta_title = '扩展-钩子列表';
-        $map = $order = $fields = array();
-        $list = $this->lists(D("Hooks")->field($fields),$map,$order);
+        $map = $fields = array();
+        $list = $this->lists(D("Hooks")->field($fields),$map);
         $thead = array(
             //元素value中的变量就是数据集中的字段,value必须使用单引号
             //查询出的数据集中的字段=>字段的表头
