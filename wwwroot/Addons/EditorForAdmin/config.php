a9d05207ca9419bea489278be84636aad390bfcc
jay <917647288@qq.com>
2013-09-12 17:40:05 +0800

    编辑器配置里添加是否允许垂直拖放编辑器 插件列表里添加分页

--- wwwroot/Addons/EditorForAdmin/config.php
+++ wwwroot/Addons/EditorForAdmin/config.php
@@ -23,5 +23,15 @@
 			'title'=>'编辑器高度:',
 			'type'=>'text',
 			'value'=>'500px'
-		)
+		),
+		'editor_resize_type'=>array(
+			'title'=>'是否允许拖拉编辑器',
+			'type'=>'radio',
+			'options'=>array(
+				'0'=>'不允许',
+				'1'=>'允许'
+			),
+			'value'=>'1',
+			'tip'=>'ubb和markdown编辑器不支持此功能'
+		),
 	);
