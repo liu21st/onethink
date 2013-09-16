d7351367fe51019164e9a20e7c81e5dd08ee7f11
huajie <banhuajie@163.com>
2013-09-16 18:41:02 +0800

    保存草稿完善

--- wwwroot/Application/Admin/Model/DocumentModel.class.php
+++ wwwroot/Application/Admin/Model/DocumentModel.class.php
@@ -478,6 +478,7 @@ class DocumentModel extends Model{
 // 				$this->error = '新增基础内容出错！';
 				return false;
 			}
+			$data['id'] = $id;
 		} else { //更新数据
 			$status = $this->save(); //更新基础内容
 			if(false === $status){
