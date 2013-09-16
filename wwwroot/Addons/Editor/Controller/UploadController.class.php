42e4decbe616436b769195fbe556431772a04176
jay <917647288@qq.com>
2013-09-16 14:09:24 +0800

    插件编辑时 钩子的排序实现，插样式修改

--- wwwroot/Addons/Editor/Controller/UploadController.class.php
+++ wwwroot/Addons/Editor/Controller/UploadController.class.php
@@ -15,9 +15,6 @@ class UploadController extends AddonsController{
 
 	/* 上传图片 */
 	public function upload(){
-		/* 返回标准数据 */
-		$return  = array('error' => 0, 'info' => '上传成功', 'data' => '');
-
 		/* 上传配置 */
 		$setting = C('EDITOR_UPLOAD');
 
@@ -26,8 +23,17 @@ class UploadController extends AddonsController{
 		$info   = $Upload->upload($_FILES);
 		$url = C('EDITOR_UPLOAD.rootPath').$info['imgFile']['savepath'].$info['imgFile']['savename'];
 		$url = str_replace('./', '/', $url);
+		$info['fullpath'] = $url;
+		return $info;
+	}
+
+	//keditor编辑器上传图片处理
+	public function ke_upimg(){
+		/* 返回标准数据 */
+		$return  = array('error' => 0, 'info' => '上传成功', 'data' => '');
+		$img = $this->upload();
 		/* 记录附件信息 */
-		if($info){
+		if($img){
 			$return['url'] = $url;
 			unset($return['info'],$return['data']);
 		} else {
@@ -38,6 +44,11 @@ class UploadController extends AddonsController{
 		/* 返回JSON数据 */
 		$this->ajaxReturn($return);
 		exit();
+	}
+
+	//ueditor编辑器上传图片处理
+	public function ue_upimg(){
+		$img = $this->upload();
 	}
 
 }
