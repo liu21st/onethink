9bbf383a82388683f2333105df3f90657d606f30
麦当苗儿 <zuojiazi.cn@gmail.com>
2013-09-16 13:56:00 +0800

    调整安装模块

--- wwwroot/Application/Common/Conf/config.php
+++ wwwroot/Application/Common/Conf/config.php
@@ -7,18 +7,14 @@
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
 
-// OneThink常量定义
-const ONETHINK_VERSION      =   '1.0beta';
-const ONETHINK_ADDON_PATH   =   './Addons/';
-
 /**
  * 系统配文件
  * 所有系统级别的配置
  */
 return array(
     /* 模块相关配置 */
-    'AUTOLOAD_NAMESPACE' => array('Addons' => ONETHINK_ADDON_PATH), //扩展模块列表
-    'DEFAULT_MODULE'     => 'Admin',
+    'AUTOLOAD_NAMESPACE' => array('Addons' => './Addons/'), //扩展模块列表
+    'DEFAULT_MODULE'     => 'Home',
     'MODULE_DENY_LIST'   => array('Common', 'User'),
 
     /* 调试配置 */
@@ -29,19 +25,18 @@ return array(
     'URL_MODEL'            => 1, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
     'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
-
+    
     /* 全局过滤配置 */
-    'DEFAULT_FILTER' => '', //过滤函数
+    'DEFAULT_FILTER' => '', //全局过滤函数
 
     /* 数据库配置 */
-    'DB_TYPE'   => '', // 数据库类型
-    'DB_HOST'   => ''/* localhost */, // 服务器地址
-    'DB_NAME'   => '', // 数据库名
-    'DB_USER'   => '', // 用户名
-    'DB_PWD'    => '', // 密码
-    'DB_PORT'   => '', // 端口
-    'DB_PREFIX' => '', // 数据库表前缀
-    'LIST_ROWS' => 15,//列表默认行数
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_NAME'   => 'onethink', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => '',  // 密码
+    'DB_PORT'   => '3306', // 端口
+    'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
     /* 文档模型配置 (文档模型核心配置，请勿更改) */
     'DOCUMENT_MODEL_TYPE' => array('专辑', '目录', '主题', '段落'),
