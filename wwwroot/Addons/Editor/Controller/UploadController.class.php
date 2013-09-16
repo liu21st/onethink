8a916fac83ac0a0f9065a5fd91cf8f7db9d4ac55
jay <917647288@qq.com>
2013-09-16 10:25:04 +0800

    添加前台上传目录配置

---
+++ wwwroot/Addons/Editor/Controller/UploadController.class.php
@@ -0,0 +1,39 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2013 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: huajie <banhuajie@163.com>
+// +----------------------------------------------------------------------
+
+namespace Addons\Editor\Controller;
+use Home\Controller\AddonsController;
+use COM\Upload;
+
+class UploadController extends AddonsController{
+
+	/* 上传图片 */
+	public function upload(){
+		/* 返回标准数据 */
+		$return  = array('status' => 1, 'info' => '上传成功', 'data' => '');
+
+		/* 上传配置 */
+		$setting = C('EDITOR_UPLOAD');
+
+		/* 调用文件上传组件上传文件 */
+		$Upload = new Upload($setting, 'Local');
+		$info   = $Upload->upload($_FILES);
+		/* 记录附件信息 */
+		if($info){
+			$return['data'] = think_encrypt(json_encode($info['attachment']));
+		} else {
+			$return['status'] = 0;
+			$return['info']   = $Upload->getError();
+		}
+
+		/* 返回JSON数据 */
+		$this->ajaxReturn($return);
+	}
+
+}
