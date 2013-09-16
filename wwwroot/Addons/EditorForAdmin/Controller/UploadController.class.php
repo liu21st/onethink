7536b99ba3a27d77e1460dd4474732f2bd38f680
jay <917647288@qq.com>
2013-09-16 18:03:24 +0800

    删除home里ke原生的上传 后台编辑器上传调整待测试

--- wwwroot/Addons/EditorForAdmin/Controller/UploadController.class.php
+++ wwwroot/Addons/EditorForAdmin/Controller/UploadController.class.php
@@ -13,25 +13,52 @@ use COM\Upload;
 
 class UploadController extends AddonsController{
 
+	public $uploader = null;
+
 	/* 上传图片 */
 	public function upload(){
-		/* 返回标准数据 */
-		$return  = array('error' => 0, 'info' => '上传成功', 'data' => '');
-
 		/* 上传配置 */
 		$setting = C('EDITOR_UPLOAD');
 
 		/* 调用文件上传组件上传文件 */
-		$Upload = new Upload($setting, 'Local');
-		$info   = $Upload->upload($_FILES);
+		$this->uploader = new Upload($setting, 'Local');
+		$info   = $this->uploader->upload($_FILES);
+		$url = C('EDITOR_UPLOAD.rootPath').$info['imgFile']['savepath'].$info['imgFile']['savename'];
+		$url = str_replace('./', '/', $url);
+		$info['fullpath'] = $url;
+		return $info;
+	}
+
+	//keditor编辑器上传图片处理
+	public function ke_upimg(){
+		/* 返回标准数据 */
+		$return  = array('error' => 0, 'info' => '上传成功', 'data' => '');
+		$img = $this->upload();
 		/* 记录附件信息 */
-		if($info){
-			$return['url'] = think_encrypt(json_encode($info['attachment']));
+		if($img){
+			$return['url'] = $img['fullpath'];
+			unset($return['info'], $return['data']);
 		} else {
-			$return['error'] = 0;
-			$return['message']   = $Upload->getError();
+			$return['error'] = 1;
+			$return['message']   = $this->uploader->getError();
 		}
 
+		/* 返回JSON数据 */
+		exit(json_encode($return));
+	}
+
+	//ueditor编辑器上传图片处理
+	public function ue_upimg(){
+
+		$img = $this->upload();
+		// file_put_contents('./img', var_export($img,1));
+		$return = array();
+		$return['url'] = $img['fullpath'];
+		$title = htmlspecialchars($_POST['pictitle'], ENT_QUOTES);
+		$return['title'] = $title;
+		$return['original'] = $img['imgFile']['name'];
+		$return['state'] = ($img)? 'SUCCESS' : $this->uploader->getError();
+		// file_put_contents('./img', var_export($return,1),FILE_APPEND);
 		/* 返回JSON数据 */
 		$this->ajaxReturn($return);
 	}
