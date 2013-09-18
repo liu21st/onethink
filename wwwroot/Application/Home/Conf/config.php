88ebe37809abd38e6123d56af1ed7d354a74763d
huajie <banhuajie@163.com>
2013-09-18 18:59:03 +0800

    缓存前缀修改为_

--- wwwroot/Application/Home/Conf/config.php
+++ wwwroot/Application/Home/Conf/config.php
@@ -16,7 +16,7 @@ return array(
     'DEFAULT_THEME' =>  'default',  // 默认模板主题名称
 
     /* 数据缓存设置 */
-    'DATA_CACHE_PREFIX' => 'onethink/', // 缓存前缀
+    'DATA_CACHE_PREFIX' => 'onethink_', // 缓存前缀
     'DATA_CACHE_TYPE'   => 'File', // 数据缓存类型
 
     /* 文件上传相关配置 */
