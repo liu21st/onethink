4545319d78209afe6554c16a90185287076accc7
yangweijie <yangweijiester@gmail.com>
2013-09-23 14:59:13 +0800

    修改糗事百科插件实现ajax刷新获取内容

--- wwwroot/Application/Common/Conf/config.php
+++ wwwroot/Application/Common/Conf/config.php
@@ -32,13 +32,14 @@ return array(
     'URL_MODEL'            => 1, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
     'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
-    
+
     /* 全局过滤配置 */
     'DEFAULT_FILTER' => '', //全局过滤函数
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '192.168.1.200', // 服务器地址
+    // 'DB_HOST'   => '192.168.1.200', // 服务器地址
+    'DB_HOST'   => 'localhost', // 服务器地址
     'DB_NAME'   => 'onethink_v01', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => '',  // 密码
