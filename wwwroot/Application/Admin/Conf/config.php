cebcada5d4c542827e7298af4843ea2ffbd4ab46
麦当苗儿 <zuojiazi.cn@gmail.com>
2013-09-29 12:33:30 +0800

    调整上传目录

--- wwwroot/Application/Admin/Conf/config.php
+++ wwwroot/Application/Admin/Conf/config.php
@@ -50,7 +50,7 @@ return array(
 
     /* 编辑器图片上传相关配置 */
     'EDITOR_UPLOAD' => array(
-		'mimes'    => 'jpg,gif,png,jpeg', //允许上传的文件MiMe类型
+		'mimes'    => '', //允许上传的文件MiMe类型
 		'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
 		'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
 		'autoSub'  => true, //自动子目录保存文件
