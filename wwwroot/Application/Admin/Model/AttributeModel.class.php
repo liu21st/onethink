6e5374c4d6785d9ab399efb02de27c80b5a8f662
huajie <banhuajie@163.com>
2013-10-24 15:20:21 +0800

    编辑属性完善

--- wwwroot/Application/Admin/Model/AttributeModel.class.php
+++ wwwroot/Application/Admin/Model/AttributeModel.class.php
@@ -27,7 +27,7 @@ class AttributeModel extends Model {
     	array('type', 'require', '数据类型必须', self::MUST_VALIDATE, 'regex', self::MODEL_BOTH),
         array('title', '1,100', '注释长度不能超过100个字符', self::VALUE_VALIDATE, 'length', self::MODEL_BOTH),
         array('remark', '1,100', '备注不能超过100个字符', self::VALUE_VALIDATE, 'length', self::MODEL_BOTH),
-    	array('model_id', 'require', '未选择操作属性的模型', self::MUST_VALIDATE, 'regex', self::MODEL_BOTH),
+    	array('model_id', 'require', '未选择操作的模型', self::MUST_VALIDATE, 'regex', self::MODEL_BOTH),
     );
 
     /* 自动完成规则 */
@@ -52,13 +52,6 @@ class AttributeModel extends Model {
             return false;
         }
 
-        //检查数据表是否存在
-        if(empty($data['model_id'])){
-        	$this->error = '未指定模型！';
-        	return false;
-        }
-//         $this->checkTableExist($data['model_id']);
-
         /* 添加或新增属性 */
         if(empty($data['id'])){ //新增属性
 
@@ -67,20 +60,28 @@ class AttributeModel extends Model {
                 $this->error = '新增属性出错！';
                 return false;
             }
+
             //新增表字段
 			$res = $this->addField($data);
 			if(!$res){
 				$this->error = '新建字段出错！';
 				return false;
 			}
+
         } else { //更新数据
+        	//更新表字段
+        	$res = $this->updateField($data);
+        	if(!$res){
+        		$this->error = '更新字段出错！';
+        		return false;
+        	}
+
             $status = $this->save();
             if(false === $status){
                 $this->error = '更新属性出错！';
                 return false;
             }
-            //更新表字段
-            $this->addField($data);
+
         }
 
         //内容添加或更新完成
@@ -89,12 +90,12 @@ class AttributeModel extends Model {
     }
 
     /**
-     * 检查当前表是否存在，没有则新建
+     * 检查当前表是否存在
      * @param intger $model_id 模型id
-     * @return boolean true 新建成功
+     * @return intger 是否存在
      * @author huajie <banhuajie@163.com>
      */
-    public function checkTableExist($model_id){
+    protected function checkTableExist($model_id){
     	$Model = M('Model');
     	//当前操作的表
 		$model = $Model->where(array('id'=>$model_id))->field('name,extend')->find();
@@ -112,10 +113,12 @@ sql;
 		return count($res);
     }
 
-    public function createTable(){
-
-    }
-
+    /**
+     * 新建表字段
+     * @param array $field 需要新建的字段属性
+     * @return boolean true 成功 ， false 失败
+     * @author huajie <banhuajie@163.com>
+     */
     protected function addField($field){
     	//检查表是否存在
     	$table_exist = $this->checkTableExist($field['model_id']);
@@ -131,7 +134,7 @@ sql;
     		$sql = <<<sql
 				CREATE TABLE IF NOT EXISTS `{$this->table_name}` (
 				`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键' ,
-				`{$field['name']}`  {$field['field']} DEFAULT '{$field['value']}' COMMENT '{$field['title']}' ,
+				`{$field['name']}`  {$field['field']} DEFAULT {$field['value']} COMMENT '{$field['title']}' ,
 				PRIMARY KEY (`id`)
 				)
 				ENGINE=MyISAM
@@ -143,6 +146,27 @@ sql;
 sql;
     	}
 
+    	$res = M()->execute($sql);
+    	return $res !== false;
+    }
+
+    /**
+     * 更新表字段
+     * @param array $field 需要更新的字段属性
+     * @return boolean true 成功 ， false 失败
+     * @author huajie <banhuajie@163.com>
+     */
+    protected function updateField($field){
+    	//检查表是否存在
+    	$table_exist = $this->checkTableExist($field['model_id']);
+
+    	//获取原字段名
+    	$last_field = $this->getFieldById($field['id'], 'name');
+
+    	$sql = <<<sql
+			ALTER TABLE `{$this->table_name}`
+CHANGE COLUMN `{$last_field}` `{$field['name']}`  {$field['field']} DEFAULT {$field['value']} COMMENT '{$field['title']}' ;
+sql;
     	$res = M()->execute($sql);
     	return $res !== false;
     }
