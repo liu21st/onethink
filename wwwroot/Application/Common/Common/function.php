759e37c70434da7c71d86d2ada440bd733ea92de
ThinkPHP <thinkphp@qq.com>
2013-10-27 09:18:19 +0800

    改进API函数支持调用其它模块的

--- wwwroot/Application/Common/Common/function.php
+++ wwwroot/Application/Common/Common/function.php
@@ -746,12 +746,13 @@ function get_model_attribute($model_id, $group = true){
 
 /**
  * 调用系统的API接口方法（静态方法）
- * @param  string  $name 格式 接口名/方法名
+ * api('/User/getName','id=5'); 调用公共模块的User接口的getName方法
+ * api('Admin/User/getName','id=5');  调用Admin模块的User接口
+ * @param  string  $name 格式 [模块名]/接口名/方法名
  * @param  array|string  $vars 参数 
- * @param  string  $module 模块名
  */
-function api($name,$vars=array(),$module='Common'){
-    list($classname,$method) =  explode('/',$name);
+function api($name,$vars=array()){
+    list($module,$classname,$method) =  explode('/',$name);
     $callback  =   $module.'\\Api\\'.$classname.'Api::'.$method;
     if(is_string($vars)) {
         parse_str($vars,$vars);
