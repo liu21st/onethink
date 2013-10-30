400036928199adb6754ac60d5a16d0b3a03cee24
yangweijie <yangweijiester@gmail.com>
2013-10-30 11:45:14 +0800

    修改权限里根据菜单表生成权限节点的方法 Admin/Controller/AdminController.class.php 里returnNodes2方法

--- wwwroot/Application/Admin/Controller/AdminController.class.php
+++ wwwroot/Application/Admin/Controller/AdminController.class.php
@@ -490,6 +490,41 @@ class AdminController extends Controller {
      *
      * @author 朱亚杰 <xcoolcc@gmail.com>
      */
+    final protected function returnNodes2($tree = true){
+        static $tree_nodes = array();
+        if ( $tree && !empty($tree_nodes[(int)$tree]) ) {
+            return $tree_nodes[$tree];
+        }
+        if((int)$tree){
+            $list = M('Menu')->field('id,pid,title,url,tip,hide')->select();
+            foreach ($list as $key => $value) {
+                if( stripos($value['url'],MODULE_NAME)!==0 ){
+                    $list[$key]['url'] = MODULE_NAME.'/'.$value['url'];
+                }
+            }
+            $nodes = list_to_tree($list,$pk='id',$pid='pid',$child='operator',$root=0);
+            foreach ($nodes as $key => $value) {
+                if(!empty($value['operator'])){
+                    $nodes[$key]['child'] = $value['operator'];
+                    unset($nodes[$key]['operator']);
+                }
+            }
+        }else{
+            $nodes = M('Menu')->field('title,url,tip')->select();
+        }
+        $tree_nodes[(int)$tree]   = $nodes;
+        return $nodes;
+    }
+
+    /**
+     * 返回后台节点数据
+     * @param boolean $tree    是否返回多维数组结构(生成菜单时用到),为false返回一维数组(生成权限节点时用到)
+     * @retrun array
+     *
+     * 注意,返回的主菜单节点数组中有'controller'元素,以供区分子节点和主节点
+     *
+     * @author 朱亚杰 <xcoolcc@gmail.com>
+     */
     final protected function returnNodes($tree = true){
         static $tree_nodes = array();
         if ( $tree && !empty($tree_nodes[(int)$tree]) ) {
