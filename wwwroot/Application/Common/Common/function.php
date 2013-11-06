0a8d34b3db5fc9316177d511cd8035148c534374
huajie <banhuajie@163.com>
2013-11-06 17:42:48 +0800

    新增“根据条件字段获取指定表的数据”方法

--- wwwroot/Application/Common/Common/function.php
+++ wwwroot/Application/Common/Common/function.php
@@ -787,3 +787,27 @@ function api($name,$vars=array()){
     }
     return call_user_func_array($callback,$vars);
 }
+
+/**
+ * 根据条件字段获取指定表的数据
+ * @param mixed $value 条件，可用常量或者数组
+ * @param string $condition 条件字段
+ * @param string $field 需要返回的字段，不传则返回整个数据
+ * @param string $table 需要查询的表
+ * @author huajie <banhuajie@163.com>
+ */
+function get_table_field($value = null, $condition = 'id', $field = null, $table = null){
+	if(empty($value) || empty($table)){
+		return false;
+	}
+
+	//拼接参数
+	$map[$condition] = $value;
+	$info = M(ucfirst($table))->where($map);
+	if(empty($field)){
+		$info = $info->field(true)->find();
+	}else{
+		$info = $info->getField($field);
+	}
+	return $info;
+}
