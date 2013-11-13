77e3489073af84a4b1d1bda33cd66ac3a5f61346
ThinkPHP <thinkphp@qq.com>
2013-11-13 09:06:26 +0800

    配置参数支持不分组 0 为不分组 需要修改原来的配置分组列表配置

--- wwwroot/Application/Admin/Controller/ConfigController.class.php
+++ wwwroot/Application/Admin/Controller/ConfigController.class.php
@@ -133,12 +133,13 @@ class ConfigController extends AdminController {
 
     // 获取某个标签的配置参数
     public function group() {
-        $id     =   I('get.id',0);
+        $id     =   I('get.id',1);
         $type   =   C('CONFIG_GROUP_LIST');
         $list   =   M("Config")->where(array('status'=>1,'group'=>$id))->field('id,name,title,extra,value,remark,type')->order('sort')->select();
         if($list) {
             $this->assign('list',$list);
         }
+		$this->assign('id',$id);
         $this->meta_title = $type[$id].'设置';
         $this->display();
     }
