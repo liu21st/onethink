bbe76c802cde040dfac7919aae51e1007e3aef75
zhuyaie <xcoolcc@gmail.com>
2013-12-18 16:48:34 +0800

    配置分组链接不生效问题

--- wwwroot/Application/Admin/Controller/ConfigController.class.php
+++ wwwroot/Application/Admin/Controller/ConfigController.class.php
@@ -23,6 +23,9 @@ class ConfigController extends AdminController {
         /* 查询条件初始化 */
         $map = array();
         $map  = array('status' => 1);
+        if(isset($_GET['group'])){
+            $map['group']  = I('get.group',0);
+        }
         if(isset($_GET['name'])){
             $map['name']  = array('like', '%'.(string)I('name').'%');
         }
