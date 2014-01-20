766c647f9c83a100385f13a17db4e82ab24a3323
ThinkPHP <thinkphp@qq.com>
2014-01-20 08:24:33 +0800

    修正文档编辑的时候创建时间为零的bug

--- wwwroot/Application/Admin/Model/DocumentModel.class.php
+++ wwwroot/Application/Admin/Model/DocumentModel.class.php
@@ -45,7 +45,7 @@ class DocumentModel extends Model{
         array('view', 0, self::MODEL_INSERT),
         array('comment', 0, self::MODEL_INSERT),
         array('extend', 0, self::MODEL_INSERT),
-        array('create_time', 'getCreateTime', self::MODEL_INSERT,'callback'),
+        array('create_time', 'getCreateTime', self::MODEL_BOTH,'callback'),
 		array('reply_time', 'getCreateTime', self::MODEL_INSERT,'callback'),
         array('update_time', NOW_TIME, self::MODEL_BOTH),
         array('status', 'getStatus', self::MODEL_BOTH, 'callback'),
