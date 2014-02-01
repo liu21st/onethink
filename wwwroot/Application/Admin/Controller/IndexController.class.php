1dbbedd1acf9999eb1c5e8312a066d69a5be5306
ThinkPHP <thinkphp@qq.com>
2014-02-01 11:06:14 +0800

    添加后台首页title

--- wwwroot/Application/Admin/Controller/IndexController.class.php
+++ wwwroot/Application/Admin/Controller/IndexController.class.php
@@ -22,6 +22,7 @@ class IndexController extends AdminController {
      */
     public function index(){
         if(UID){
+            $this->meta_title = '管理首页';
             $this->display();
         } else {
             $this->redirect('Public/login');
