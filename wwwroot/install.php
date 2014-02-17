3787977efc5400f9d4f84ae38a2cfc33ac89abee
thinkphp <thinkphp@qq.com>
2014-02-17 10:14:22 +0800

    修正一处错误

--- wwwroot/install.php
+++ wwwroot/install.php
@@ -9,14 +9,12 @@
 
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
-$_GET['m'] = 'Install';
-
 /**
  * 系统调试设置
  * 项目正式部署后请设置为false
  */
 define ( 'APP_DEBUG', true );
-
+define ( 'BIND_MODULE','Install');
 /**
  * 应用目录设置
  * 安全期间，建议安装调试完成后移动到非WEB目录
