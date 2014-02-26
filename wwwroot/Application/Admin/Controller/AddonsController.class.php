87cba6bf247f9b1b3383c6cc2466a3a2b908d885
yangweijie <917647288@qq.com>
2014-02-26 15:37:04 +0800

    修复插件后台新增删除时模型里字段读取显示的不正确

--- wwwroot/Application/Admin/Controller/AddonsController.class.php
+++ wwwroot/Application/Admin/Controller/AddonsController.class.php
@@ -190,28 +190,28 @@ use Think\Model;
  */
 class {$data['info']['name']}Model extends Model{
     public \$model = array(
-        'title'=>'',
-        'template_add'=>'',
-        'template_edit'=>'',
-        'search_key'=>'',
+        'title'=>'',//新增[title]、编辑[title]、删除[title]的提示
+        'template_add'=>'',//自定义新增模板自定义html edit.html 会读取插件根目录的模板
+        'template_edit'=>'',//自定义编辑模板html
+        'search_key'=>'',// 搜索的字段名，默认是title
         'extend'=>1,
     );
 
     public \$_fields = array(
         'id'=>array(
-            'name'=>'id',
-            'title'=>'ID',
-            'type'=>'num',
-            'remark'=>'',
-            'is_show'=>2,
-            'value'=>0,
+            'name'=>'id',//字段名
+            'title'=>'ID',//显示标题
+            'type'=>'num',//字段类型
+            'remark'=>'',// 备注，相当于配置里的tip
+            'is_show'=>3,// 1-始终显示 2-新增显示 3-编辑显示 0-不显示
+            'value'=>0,//默认值
         ),
         'title'=>array(
             'name'=>'title',
             'title'=>'书名',
             'type'=>'string',
             'remark'=>'',
-            'is_show'=>3,
+            'is_show'=>1,
             'value'=>0,
             'is_must'=>1,
         ),
