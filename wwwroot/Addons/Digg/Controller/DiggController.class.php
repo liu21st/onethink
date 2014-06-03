fc0f4cc5170802ab18675eea4a3d5608220fb5a2
yangweijie <917647288@qq.com>
2014-06-03 09:37:17 +0800

    更新样式 修复js变量拼写错误 修复sqllike查询

--- wwwroot/Addons/Digg/Controller/DiggController.class.php
+++ wwwroot/Addons/Digg/Controller/DiggController.class.php
@@ -11,7 +11,7 @@ class DiggController extends AddonsController{
 		$uid = is_login();
 		if(!$uid)
 			$this->error('请先登录再投票');
-		$has_vote = M('Digg')->where("document_id={$id} AND uids like ',{$uid},'")->find();
+		$has_vote = M('Digg')->where("document_id={$id} AND uids like '%,{$uid},%'")->find();
 		if(!$has_vote){
 			$field = $type == '1' ? 'good' : 'bad';
 			$data = array($field=>array('exp',"$field+1"), "uids"=>array('exp',"concat(uids,'$uid,')"));
