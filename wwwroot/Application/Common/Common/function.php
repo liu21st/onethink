465d463ebfb848c5ae316cff3c92e8f1fa6c995c
hottredpen <hottredpen@126.com>
2014-07-25 22:11:08 +0800

    添加get_top_model，获取顶级模型，为扩展其他模型准备
    添加get_top_model，获取顶级模型，为扩展其他模型准备，其他修改在admin/view/modle/add.html的32行左右，及其admin/view/modle/edit.html的36行左右

--- wwwroot/Application/Common/Common/function.php
+++ wwwroot/Application/Common/Common/function.php
@@ -495,7 +495,17 @@ function get_category_name($id){
 function get_category_title($id){
     return get_category($id, 'title');
 }
-
+/**
+ * 获取顶级模型信息
+ */
+function get_top_model(){
+        $map   = array('status' => 1, 'extend' => 0);
+        $model = M('Model')->where($map)->field(true)->select();
+        foreach ($model as $value) {
+            $list[$value['id']] = $value;
+        }
+		return $list;
+}
 /**
  * 获取文档模型信息
  * @param  integer $id    模型ID
@@ -517,7 +527,7 @@ function get_document_model($id = null, $field = null){
 
     /* 获取模型名称 */
     if(empty($list)){
-        $map   = array('status' => 1, 'extend' => 1);
+        $map   = array('status' => 1, 'extend' => array('gt',0));
         $model = M('Model')->where($map)->field(true)->select();
         foreach ($model as $value) {
             $list[$value['id']] = $value;
