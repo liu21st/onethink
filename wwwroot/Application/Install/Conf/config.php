d729560425e22ebbb16e3c7bbee919d15d01bd0a
ThinkPHP <thinkphp@qq.com>
2014-07-26 09:28:59 +0800

    修正安装完成后 后台和前台访问地址出错的问题

--- wwwroot/Application/Install/Conf/config.php
+++ wwwroot/Application/Install/Conf/config.php
@@ -15,6 +15,12 @@ define('INSTALL_APP_PATH', realpath('./') . '/');
 
 return array(
     
+    /* URL配置 */
+    'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
+    'URL_MODEL'            => 3, //URL模式 使用兼容模式安装
+    'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
+    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
+
     'ORIGINAL_TABLE_PREFIX' => 'onethink_', //默认表前缀
 
     /* 模板相关配置 */
