8be76eedf277a192f6fb71a2009a58786436e56c
ThinkPHP <thinkphp@qq.com>
2014-08-09 18:33:54 +0800

    开发版版本号更新

--- wwwroot/Application/Common/Common/function.php
+++ wwwroot/Application/Common/Common/function.php
@@ -8,7 +8,7 @@
 // +----------------------------------------------------------------------
 
 // OneThink常量定义
-const ONETHINK_VERSION    = '1.1.140726';
+const ONETHINK_VERSION    = '1.1.140809';
 const ONETHINK_ADDON_PATH = './Addons/';
 
 /**
