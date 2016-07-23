312a6ca768d1fd23805229f9f193153fe27d2288
hsp55 <hsp55@qq.com>
2016-07-23 09:43:24 +0800

    修改自动验证及自动完成

--- application/admin/model/Menu.php
+++ application/admin/model/Menu.php
@@ -16,15 +16,24 @@ use think\Model;
 
 class Menu extends Model {
 
-//    protected $_validate = array(
-//        array('title','require','标题必须填写'),
-//        array('url','require','链接必须填写'),
-//    );
+    /* 自动完成规则 */
+	protected $auto = ['title'];
+	protected $insert = ['status'=>1];
 
-//    /* 自动完成规则 */
-//    protected $_auto = array(
-//        array('title', 'htmlspecialchars', self::MODEL_BOTH, 'function'),
-//        array('status', '1', self::MODEL_INSERT),
-//    );
+	protected function setTitleAttr($value) {
+		return htmlspecialchars($value);
+	}
+
+    /* 自动验证规则 */
+	protected $validate = [
+		'rule' => [
+			'title'  => 'require',
+			'url' => 'require',
+		],
+		'msg' => [
+			'title.require' => '标题必须填写',
+			'url.require' => '链接必须填写',
+		]
+	];
 
 }
