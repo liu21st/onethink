908d0f27cc706ecc8ac887cf3583308c4a345016
hsp55 <hsp55@qq.com>
2016-07-28 21:35:44 +0800

    导航管理可用

--- application/admin/model/Channel.php
+++ application/admin/model/Channel.php
@@ -14,15 +14,24 @@ use think\Model;
  */
 
 class Channel extends Model {
-    protected $_validate = array(
-        array('title', 'require', '标题不能为空', self::MUST_VALIDATE , 'regex', self::MODEL_BOTH),
-        array('url', 'require', 'URL不能为空', self::MUST_VALIDATE , 'regex', self::MODEL_BOTH),
-    );
+	//自动写入时间戳字段
+	protected $autoWriteTimestamp = true;
 
-    protected $_auto = array(
-        array('create_time', NOW_TIME, self::MODEL_INSERT),
-        array('update_time', NOW_TIME, self::MODEL_BOTH),
-        array('status', '1', self::MODEL_INSERT),
-    );
+	/* 自动完成规则 */
+	// protected $auto = [];
+	protected $insert = ['status'=>1];
+	// protected $update = [];
+
+	/* 自动验证规则 */
+	protected $validate = [
+		'rule' => [
+			'title'  => 'require',
+			'url' => 'require',
+		],
+		'msg' => [
+			'title.require' => '标题不能为空',
+			'url.require' => 'URL不能为空',
+		]
+	];
 
 }
